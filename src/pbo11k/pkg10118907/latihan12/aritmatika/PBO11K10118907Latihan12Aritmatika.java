6d643756c9518d90647f53ea002b650f70af8ec9
unknown <fadlullahyazid@gmail.com>
2019-09-24 23:28:24 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan12/aritmatika/PBO11K10118907Latihan12Aritmatika.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan12.aritmatika;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan aritmatika
+ * 
+ */
+public class PBO11K10118907Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        
+        System.out.println("a + b = "+ (a + b));
+        System.out.println("a - b = "+ (a - b));
+        System.out.println("a * b = "+ (a * b));
+        System.out.println("b / s = "+ (b / a));
+        System.out.println("b % a = "+ (b % a));
+        System.out.println("c % a = "+ (c % a));
+        System.out.println("a++ = "+ (a++));
+        System.out.println("b-- = "+ (b--));
+        System.out.println("d++  b = "+ (d++));
+        System.out.println("++d "+ (++d));
+    }
+    
+}
